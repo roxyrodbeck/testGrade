d7e129de895de7d9c016592b147b5f893c275f48
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-09 23:36:05 -0600

    Update main.js
    Change all the is to testGrade and removed the loop.

--- main.js
+++ main.js
@@ -8,16 +8,15 @@
 // 90 to 100 is an "A"
 
 let testGrade = Math.floor(Math.random() * 101); 
-    for (i = 0; i < testGrade; i++) {
-       if (i < 65) {
+    // let testGrade = 69;
+       if (0 <= testGrade && testGrade < 65) {
         console.log("F");
-       } else if (64 < i && i < 71) {
+       } else if (64 < testGrade && testGrade < 71) {
         console.log("D");
-    } else if (70 < i && i < 80) {
+    } else if (70 < testGrade && testGrade < 80) {
         console.log("C");
-    } else if (79 < i && i < 90) {
+    } else if (79 < testGrade && testGrade < 90) {
         console.log("B");
-    } else if (89 < i && i <= 100) {
+    } else if (89 < testGrade && testGrade <= 100) {
         console.log("A");
-    }
     }
